d4038da6f2f34eeee5525477b4251ff6f65a8345
Jason Gelsomino <jgelsomino@foundry360.us>
2025-08-27 17:48:38 +0000

    Make that the default landing page for the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
-import { RevOpsAnalyzer } from '@/components/revops-analyzer';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
 export default function Home() {
-  return <RevOpsAnalyzer />;
+  return (
+    <div className="flex flex-col gap-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>Welcome to your new App!</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>This is the homepage. You can start building your application here.</p>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
