d01b1e0cb8c501f7c9302adb4ca2f2154b5e545c
Jason Gelsomino <jgelsomino@foundry360.us>
2025-08-27 19:18:27 +0000

    Still have the hydration issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="font-body antialiased h-full" suppressHydrationWarning>
+      <body className="font-body antialiased h-full">
         {children}
         <Toaster />
       </body>
