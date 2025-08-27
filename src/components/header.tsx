a4bf23d08c7dc6e090e2400a4966ab2afcf9d9bc
Jason Gelsomino <jgelsomino@foundry360.us>
2025-08-27 18:43:34 +0000

    Change the border color of the header to dark blue

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { Logo } from './logo';
 
 export function Header() {
   return (
-    <header className="flex h-16 items-center gap-4 border-b border-sidebar-border bg-sidebar text-sidebar-foreground px-6">
+    <header className="flex h-16 items-center gap-4 border-b border-sidebar bg-sidebar px-6 text-sidebar-foreground">
       <Logo />
     </header>
   );
