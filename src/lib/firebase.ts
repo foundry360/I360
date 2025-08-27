3a12b74d282e58160d6cc025c24c508d9148f638
Jason Gelsomino <jgelsomino@foundry360.us>
2025-08-27 19:56:14 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  "projectId": "insights360-ta9hn",
+  "appId": "1:249056251135:web:016bba83b9d0d0150f50ca",
+  "storageBucket": "insights360-ta9hn.firebasestorage.app",
+  "apiKey": "AIzaSyC_Vd2Ttd3VwzefYCdTNLuO4Dk7KDESOE4",
+  "authDomain": "insights360-ta9hn.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "249056251135"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+
+export { app, db };
