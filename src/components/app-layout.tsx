7bb2c00a434f54c9acc8a914b27a569b4b0474fe
Jason Gelsomino <jgelsomino@foundry360.us>
2025-08-27 18:58:41 +0000

    The main content area should consume all the area out side of the header

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -4,13 +4,13 @@ import { Sidebar } from '@/components/sidebar';
 
 export function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col bg-sidebar">
+    <div className="flex h-screen flex-col">
       <Header />
       <div className="flex flex-1 overflow-hidden">
         <Sidebar />
-        <div className="flex-1 overflow-y-auto p-4 md:p-6">
-          <main className="h-full rounded-lg bg-background p-4 md:p-6">{children}</main>
-        </div>
+        <main className="flex-1 overflow-y-auto bg-sidebar p-4 md:p-6">
+          <div className="h-full rounded-lg bg-background p-4 md:p-6">{children}</div>
+        </main>
       </div>
     </div>
   );
