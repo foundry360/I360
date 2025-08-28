0390fcdc2c66cc6cd49c63be50f37d16a39ef997
Jason Gelsomino <jgelsomino@foundry360.us>
2025-08-28 21:42:02 +0000

    Didn't work

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -7,9 +7,9 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-full flex-col">
       <Header />
-      <div className="flex flex-1 overflow-hidden">
+      <div className="flex flex-1 overflow-hidden bg-sidebar">
         <Sidebar />
-        <main className="flex-1 overflow-y-auto p-4 md:p-6 bg-background rounded-tl-lg">
+        <main className="flex-1 overflow-y-auto bg-background p-4 md:p-6 rounded-tl-lg">
           {children}
         </main>
       </div>
