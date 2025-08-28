d84427f0184bace578b6df4653dda3d5f3627ae4
Jason Gelsomino <jgelsomino@foundry360.us>
2025-08-28 21:41:12 +0000

    That didn't work. How can we do it? Do we need to add a container?

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -5,11 +5,11 @@ import { Sidebar } from '@/components/sidebar';
 
 export function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-full flex-col bg-background">
+    <div className="flex h-full flex-col">
       <Header />
       <div className="flex flex-1 overflow-hidden">
         <Sidebar />
-        <main className="flex-1 overflow-y-auto p-4 md:p-6 rounded-tl-lg">
+        <main className="flex-1 overflow-y-auto p-4 md:p-6 bg-background rounded-tl-lg">
           {children}
         </main>
       </div>
