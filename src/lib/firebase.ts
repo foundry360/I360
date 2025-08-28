ba41b3c0e904f61226996d80028a2a1870f4bdb8
Jason Gelsomino <jgelsomino@foundry360.us>
2025-08-28 16:20:41 +0000

    Nope

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -11,7 +11,6 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
