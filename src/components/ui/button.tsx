f39fcf656f7a3e9092d39557583d428a96895711
Jason Gelsomino <jgelsomino@foundry360.us>
2025-09-09 13:20:36 +0000

    Didn't change and we have an error: Error: Cannot read properties of nul

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,4 +1,5 @@
 
+
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
@@ -59,6 +60,7 @@ export { Button, buttonVariants }
 
 
     
+
 
 
 
