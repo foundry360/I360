1b5d375851b6df1d7b428b8c2e87a5d96ccdaddd
Jason Gelsomino <jgelsomino@foundry360.us>
2025-09-09 00:56:27 +0000

    Change it to 0°, 0%, 75%

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -542,8 +542,8 @@ export default function DashboardPage() {
                 onClick={openNewProjectDialog}
               >
                 <div className="flex flex-col items-center gap-2 text-muted-foreground">
-                  <PlusCircle className="w-12 h-12 text-[hsl(0,0%,85%)] dark:text-foreground/10" />
-                  <p className="text-sm text-[hsl(0,0%,85%)] dark:text-foreground/10">New Engagement</p>
+                  <PlusCircle className="w-12 h-12 text-[hsl(0_0%_75%)] dark:text-foreground/10" />
+                  <p className="text-sm text-[hsl(0_0%_75%)] dark:text-foreground/10">New Engagement</p>
                 </div>
             </Card>
         </div>
