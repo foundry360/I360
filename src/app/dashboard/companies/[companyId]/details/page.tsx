6fcbfb5088308a37baa19d8be608073e17f76971
Jason Gelsomino <jgelsomino@foundry360.us>
2025-09-10 13:46:54 +0000

    Add some space above and below the divider for each section in the side

--- src/app/dashboard/companies/[companyId]/details/page.tsx
+++ src/app/dashboard/companies/[companyId]/details/page.tsx
@@ -714,7 +714,7 @@ export default function CompanyDetailsPage() {
               <Card className="h-full border-t-0 border-r-0 border-b-0 rounded-none">
                 <CardContent className="p-4">
                   <Accordion type="multiple" defaultValue={['item-1', 'item-2', 'item-3', 'item-4']} className="w-full">
-                    <AccordionItem value="item-1">
+                    <AccordionItem value="item-1" className="py-2">
                       <div className="flex items-center justify-between w-full">
                         <AccordionTrigger chevronFirst className="flex-1">
                             <h3 className="font-semibold">Company Information</h3>
@@ -742,7 +742,7 @@ export default function CompanyDetailsPage() {
                           </div>
                       </AccordionContent>
                     </AccordionItem>
-                    <AccordionItem value="item-2">
+                    <AccordionItem value="item-2" className="py-2">
                        <div className="flex items-center justify-between w-full">
                           <AccordionTrigger chevronFirst className="flex-1">
                               <h3 className="font-semibold">Primary Contacts</h3>
@@ -775,7 +775,7 @@ export default function CompanyDetailsPage() {
                         </div>
                       </AccordionContent>
                     </AccordionItem>
-                    <AccordionItem value="item-3">
+                    <AccordionItem value="item-3" className="py-2">
                        <div className="flex items-center justify-between w-full">
                           <AccordionTrigger chevronFirst className="flex-1">
                               <h3 className="font-semibold">Company Engagements</h3>
@@ -806,7 +806,7 @@ export default function CompanyDetailsPage() {
                           </div>
                       </AccordionContent>
                     </AccordionItem>
-                     <AccordionItem value="item-4">
+                     <AccordionItem value="item-4" className="py-2">
                       <div className="flex items-center justify-between w-full">
                         <AccordionTrigger chevronFirst className="flex-1">
                             <h3 className="font-semibold">Recent Activity</h3>
